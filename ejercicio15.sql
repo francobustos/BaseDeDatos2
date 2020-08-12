79dd6540ad96b0807e744fc01d2405a7574c618a
francobustos <franco.bustos60@gmail.com>
2020-08-12 17:13:33 -0300

    ejercicio 4,5 y 6 de la clase 15

--- ejercicio15.sql
+++ ejercicio15.sql
@@ -39,5 +39,50 @@ JOIN inventory USING (film_id)
 JOIN rental r USING (inventory_id)
 GROUP BY 1;
 
+/*
+Create a view called actor_information where it should return, 
+actor id, 
+first name, 
+last name and 
+the amount of films he/she acted on.
+*/
+CREATE OR REPLACE VIEW actor_information AS
+SELECT actor_id, first_name, last_name, COUNT(film_id) AS 'films_acted'
+FROM actor
+JOIN film_actor USING (actor_id)
+GROUP BY 1;
 
+
+/*
+Analyze view actor_info, explain the entire query and specially how the sub query works. 
+Be very specific, take some time and decompose each part and give an explanation for each.
+*/
+
+/*CREATE OR REPLACE crea la view
+ALGORITHM = UNDEFINED VIEW `actor_info` AS la llama actor_film
+select
+    `a`.`actor_id` AS `actor_id`, muestra el id del actor
+    `a`.`first_name` AS `first_name`, muestra el nombre del actor
+    `a`.`last_name` AS `last_name`, muestra el apellido del actor
+    group_concat(distinct concat(`c`.`name`, ': ',(select group_concat(`f`.`title` order by `f`.`title` ASC separator ', ') 
+    												from ((`film` `f` join `film_category` `fc` on((`f`.`film_id` = `fc`.`film_id`))) 
+    												join `film_actor` `fa` on((`f`.`film_id` = `fa`.`film_id`))) 
+    												where ((`fc`.`category_id` = `c`.`category_id`) and (`fa`.`actor_id` = `a`.`actor_id`))))
+order by
+    `c`.`name` ASC separator '; ') AS `film_info`  Muestra las peliculas en las que trabajo el actor con su correspondiente categoria
+from
+    (((`actor` `a`
+left join `film_actor` `fa` on
+    ((`a`.`actor_id` = `fa`.`actor_id`)))
+left join `film_category` `fc` on
+    ((`fa`.`film_id` = `fc`.`film_id`)))
+left join `category` `c` on
+    ((`fc`.`category_id` = `c`.`category_id`))) Une al actor con su correspondiente pelicula y categoria
+group by
+    `a`.`actor_id`,
+    `a`.`first_name`,
+    `a`.`last_name`;*/
+
+-- Materialized views, write a description, why they are used, alternatives, DBMS were they exist, etc
+-- No entendi
 
